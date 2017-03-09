4031f8aabebbb7b4e7a3c52ab9aa90fabb152026
bsokat <barissokat@gmail.com>
2017-03-09 14:24:53 +0300

    Create User

---
+++ MembershipSystem/Controllers/MembershipController.cs
@@ -0,0 +1,39 @@
+﻿using MembershipSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MembershipSystem.Controllers
+{
+    public class MembershipController : Controller
+    {
+        ApplicationDbContext _db = new ApplicationDbContext();
+        // GET: Membership
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CreateUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateUser(User newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = newUser.Name
+                };
+                _db.Users.Add(user);
+                _db.SaveChanges();
+            }
+            return View();
+        }
+    }
+}
---
+++ MembershipSystem/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MembershipSystem.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+
+        [Required, Display(Name = "User Name")]
+        public string Name { get; set; }
+    }
+}
